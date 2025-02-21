f239361028cd0ecca151120f986e78cefb15a3a6
Frederik Berger <frederik@MacBookAir.fritz.box>
2025-02-21 20:11:52 +0100

    Freddy hat sich ausgetobt

--- Age-Model/src/age_model_plot.R
+++ Age-Model/src/age_model_plot.R
@@ -30,24 +30,35 @@ combined_plot <- ggplot() +
   geom_line(data=data, aes(x=age_old..a.BP..1, y=depth_max - (sed.rate_old..m.ka. * scale_factor)), color="blue", linetype="dotted", size=1) +
   
   # X-Axis at the Top
-  scale_x_continuous(position = "top") +
+  scale_x_continuous(position = "top", breaks = seq(0, 30000, by = 5000)) +
   
   # Primary Y-Axis (Depth) -> Reversed
-  scale_y_reverse(name="Depth (cm)", limits=c(depth_max, depth_min - 50 ),
-                  sec.axis = sec_axis(~ (depth_max - .) / scale_factor, 
-                                      name="Sedimentation Rate (m/ka)", 
-                                      breaks=seq(sed_rate_min - 50, sed_rate_max, by=1), 
-                                      labels=seq(sed_rate_min - 50, sed_rate_max, by=1))) +  # Fixed from 0 to 7
-  
+  scale_y_reverse(
+    name="depth [cm]",
+    limits=c(depth_max, depth_min - 50 ),
+    breaks = seq(0, 800, by = 100),
+    minor_breaks = seq(50, 650, by = 100),
+    sec.axis = sec_axis(
+      ~ (depth_max - .) / scale_factor, 
+      name="sedimentation rate [m/ka]", 
+      breaks=seq(sed_rate_min - 50, sed_rate_max, by=1), 
+      labels=seq(sed_rate_min - 50, sed_rate_max, by=1)
+    )
+  ) +
+ 
   # Labels and themes
-  labs(x="Age (a BP)", title="Downcore Age-Depth & Sedimentation Rate") +
+  labs(x="age [cal yr B.P.]", title="Age Model and Sedimentation Rate") +
   theme_minimal() +
   theme(
-    axis.text.x = element_text(size=12),  
-    axis.text.y = element_text(size=12),
+    plot.title = element_text(size = 16, face = "bold", hjust = 0.5),  # Center the title
+    axis.text.x = element_text(size = 12, face = "bold"),  # Bold and size for x-axis text
+    axis.text.y = element_text(size = 12, face = "bold"),  # Bold and size for y-axis text
+    axis.title.x = element_text(size = 14, face = "bold"), # Bold and size for x-axis label
+    axis.title.y = element_text(size = 14, face = "bold"), # Bold and size for y-axis label
     panel.grid.major = element_line(color = "grey80", size = 0.5),
-    panel.grid.minor = element_line(color = "grey90", size = 0.3),
+    panel.grid.minor = element_line(color = "grey90", size = 0.3)
   )
 
 # Display the combined plot
 combined_plot
+
